eb4d9aaae7702f3958d7ce3261a3fb22aa279734
Mitchel Klijn <mitchelklijn2004@gmail.com>
2022-05-23 11:57:51 +0200

    EnumOpdracht toegevoegd

---
+++ Csharp2Opdrachten/EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EnumOpdracht
+{
+    class Program
+    {
+        enum Colors
+        {
+            Red,
+            Orange,
+            Yellow,
+            Green
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
